e10a1d335b0e339311bfe238339384e58bc6d8d7
Christopher Mays <cpmays2018@gmail.com>
2020-07-29 11:09:22 -0500

    Uploading files/Initial commit

---
+++ JS/code.js
@@ -0,0 +1,9 @@
+//loads ticket purchase page
+$(".ticketBtn").click(function() {
+  window.location.href = "form.html";
+});
+
+//
+$(".confirmBtn").click(function() {
+  alert("Thank you for your purchse. Your tickets will be sent to your email.");
+});
